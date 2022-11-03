5c823b31b5f3a108cce4711c1f4fd8772446832e
Kiana-Alize Diaz <44187476+kiana51@users.noreply.github.com>
2022-11-03 14:56:44 -0400

    logo added to signUp/signIn

---
+++ .history/root/frontend/Router_20221030180604.js
@@ -0,0 +1,78 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Text, HamburgerIcon, Pressable, Center } from "native-base";
+
+import SignUpScreen from "./pages/SignUpScreen.js";
+import SignInScreen from "./pages/SignInScreen.js";
+import HomeScreen from "./pages/HomeScreen.js";
+import 
+
+import { useAuth } from "./contexts/Auth";
+import { useEffect } from "react";
+
+function Loading() {
+  return (
+    <Center>
+      <Text>Loading</Text>
+    </Center>
+  );
+}
+
+function AuthStack(props) {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Sign Up" component={SignUpScreen} />
+      <Stack.Screen name="Sign In" component={SignInScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function AppStack(props) {
+  const Stack = createNativeStackNavigator();
+  const { loadUser } = useAuth();
+  useEffect(() => {
+    loadUser;
+  }, []);
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "Outfitter",
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#1e40af",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontFamily: "sans-serif-medium",
+          },
+          headerLeft: () => <></>,
+          headerRight: () => (
+            <Pressable>
+              <HamburgerIcon size="md" color="white" />
+            </Pressable>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function Router() {
+  const { signedIn, loading } = useAuth();
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <NavigationContainer>
+      {signedIn ? <AppStack /> : <AuthStack />}
+    </NavigationContainer>
+  );
+}
---
+++ .history/root/frontend/config_20221103021301.js
@@ -0,0 +1,5 @@
+import { API_URL } from "'re";
+
+export default {
+  API_URL,
+};
