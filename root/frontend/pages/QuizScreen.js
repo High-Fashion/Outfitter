fbfd8af1dd750ba981379a717db4c16ce7e6d285
Kiana-Alize Diaz <44187476+kiana51@users.noreply.github.com>
2022-11-06 23:02:18 -0500

    mixed setup screen quiz with home screen quiz

--- root/frontend/pages/QuizScreen.js
+++ root/frontend/pages/QuizScreen.js
@@ -1,36 +1,102 @@
 /*https://hossein-zare.github.io/react-native-dropdown-picker-website/docs/usage*/
 /*https://snack.expo.dev/@mali_ai/react-native-dropdown-picker*/
 import { useAuth } from "../contexts/Auth";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import {
   Text,
   View,
   TouchableOpacity,
+  VStack,
+  HStack,
+  Button,
+  Input,
+  HamburgerIcon,
 } from "native-base";
-import {useForm, Controller} from 'react-hook-form';
-
 import DropDownPicker from 'react-native-dropdown-picker';
 
-function QuizScreen({route}) {
-
+function QuizScreen({ navigation: { navigate }, route }) {
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState([]);
+    const [currentInput, setInput] = useState();
     const [items, setItems] = useState([
       {label: 'Emo', value: 'emo'},
       {label: 'Goth', value: 'goth', parent: 'emo'},
+      {label: 'Punk', value: 'punk', parent: 'emo'},
 
       {label: 'Urban', value: 'urban'},
       {label: 'Hipster', value: 'hipster', parent: 'urban'},
       {label: 'Vintage', value: 'vintage', parent: 'urban'},
-    ]);
-    const { user } = useAuth();
-    const { handleSubmit, control } = useForm();
-    const onSubmit = (data) => {
-      console.log(data, "data");
-    };
+      {label: 'Streetwear', value: 'streetwear', parent: 'urban'},
 
-    return (
-      <View>{user.firstName && <Text>{ user.firstName}, Check out our style quiz</Text>} 
+      {label: 'Bohemian', value: 'bohemian'},
+
+      {label: 'Surfer', value: 'surfer'},
+      {label: 'Beach', value: 'beach', parent: 'surfer'},
+      {label: 'Country', value: 'country'},
+
+      {label: 'Geeky', value: 'geeky'},
+      {label: 'nerdy', value: 'nerdy', parent: 'geeky'},
+    ]);
+
+    const updateValue = (text) => {
+        if (!text.includes("\n")) {
+          setInput(text);
+          return;
+        }
+        if (text == "\n") {
+          setInput("");
+          return;
+        }
+        setInput("");
+        if (!styles.includes(text.replace(/(\r\n|\n|\r)/gm, "")))
+          setStyles([...styles, text.replace(/(\r\n|\n|\r)/gm, "")]);
+      };
+
+      const removeStyle = (style) => {
+        setStyles(
+          styles.filter((value) => {
+            return value != style;
+          })
+        );
+      };
+
+    const { user } = useAuth();
+    const [styles, setStyles] = useState([]);
+    const [completed, setCompleted] = useState(false);
+    
+      function complete(name) {
+        const temp = completed;
+        temp[name] = true;
+        setCompleted(temp);
+      }
+    
+      const [setupComplete, setSetupComplete] = useState(false);
+
+
+  useEffect(() => {
+    console.log("key");
+    var complete = true;
+    Object.keys(completed).forEach((key) => {
+      console.log(key + " " + completed[key]);
+      if (!completed[key]) complete = false;
+    });
+    setSetupComplete(complete);
+  });
+
+  async function finishSetup() {
+    if (!setupComplete) return;
+    var finished = await route.params.finish({
+      styles: styles,
+    });
+    if (finished) {
+      navigate("Media");
+    }
+  }
+
+
+    return(
+     <View>{user.firstName && <Text>{ user.firstName}, How would you describe your style?</Text>} 
+       <VStack my={5} justifyContent="space-evenly" flex={1}>
       <View style={{
         flex: 1,
         alignItems: 'center',
@@ -50,8 +116,33 @@ function QuizScreen({route}) {
           mode="BADGE"
           placeholder="Select Style"
         />
-      </View>
-      </View>
+
+
+        <HStack space={1}>
+        {styles.map((style) => (
+          <StyleTag style={style} remove={() => removeStyle(style)} />
+        ))}
+      </HStack>
+      <Input
+        multiline={true}
+        placeholder="Input style tags"
+        value={currentInput}
+        onChangeText={(text) => updateValue(text)}
+      />
+        </View>
+        </VStack>
+        <Button
+        isDisabled={!setupComplete}
+        onPress={() => {
+          styles: styles;
+          complete(styles);
+          setStyles(styles);
+          finishSetup();
+        }}
+      >
+        <Text>Finish</Text>
+      </Button>
+     </View>
     );
 }
 
